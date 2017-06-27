8026a37203b333efe55455a06ffbe764653a2347
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-06-26 18:59:18 -0700

    Added default look and feel

--- MineSweeper.java
+++ MineSweeper.java
@@ -5,9 +5,32 @@
  *  Created based off of the PowaySoft TicTacToe Assignment
  */
 
+// Import to support default look and feel
+import javax.swing.UIManager; 
+import javax.swing.UnsupportedLookAndFeelException; 
+
 // main() 
 public class MineSweeper {
     public static void main(String[] args){ 
+        
+        // Set look and feel default
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            // handle exception
+        }
+        catch (InstantiationException e) {
+            // handle exception
+        }
+        catch (IllegalAccessException e) {
+            // handle exception
+        }
+        
         Game mineSweeper = new Game();
         UI display = new UI(mineSweeper);
         mineSweeper.setDisplay(display);
